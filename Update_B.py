cea27c5e0cf7807533f555e535411c597741ab37
Prashanth-staar <56891492+Prashanth-staar@users.noreply.github.com>
2025-01-27 18:10:54 +0530

    Update Update_B.py

--- Update_B.py
+++ Update_B.py
@@ -1,2 +1,3 @@
 # now i am adding to see if I can pull all the existing request
 print(" verifying to see if I can pull the over all child branch to main ")
+print(" not showing the pull request, since this was reverted, I guess we need to work with the new reverted one ")
